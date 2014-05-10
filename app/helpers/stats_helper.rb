4ffffaeb583d30dcce50ae54303f90cf64b457bc
Steven Schmid <steschmid@gmx.ch>
2014-05-10 11:25:18 +0200

    NaN

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -1,6 +1,10 @@
 module StatsHelper
   def win_rate(num_wins, num_losses)
-    ratio = num_wins.to_f / (num_wins + num_losses)
+    ratio = if num_wins + num_losses > 0
+              num_wins.to_f / (num_wins + num_losses)
+            else
+              0
+            end
     (ratio * 100.0).round(1).to_s + '%'
   end
 end
