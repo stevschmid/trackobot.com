4be0e2ecdfc8675e381f0d911ac49fea12d70401
Steven Schmid <steschmid@gmx.ch>
2014-06-12 02:13:04 +0200

    fix sorting (I feel dirty)

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -32,34 +32,41 @@ class StatsController < ApplicationController
     user_arenas = current_user.arenas
     user_arenas = user_arenas.where('arenas.created_at >= ?', min_date_for_time_range(@time_range)) if @time_range
 
-    [@as_hero || Hero.all].flatten.each do |hero|
-      as_results = user_results.where(hero_id: hero.id)
-      as_results = as_results.where(opponent_id: @vs_hero.id) if @vs_hero
-      @stats[:classes][:as][hero.name] = { wins: as_results.wins.count, losses: as_results.losses.count }
-    end
+    @stats[:classes][:as] = Hash[
+      [@as_hero || Hero.all].flatten.collect do |hero|
+        as_results = user_results.where(hero_id: hero.id)
+        as_results = as_results.where(opponent_id: @vs_hero.id) if @vs_hero
+        [hero.name, {wins: as_results.wins.count, losses: as_results.losses.count}]
+      end.sort_by { |_, hero_stats| win_rate(hero_stats[:wins], hero_stats[:losses]) }
+    ]
 
-    [@vs_hero || Hero.all].flatten.each do |hero|
-      vs_results = user_results.where(opponent_id: hero.id)
-      vs_results = vs_results.where(hero_id: @as_hero.id) if @as_hero
-      @stats[:classes][:vs][hero.name] = { wins: vs_results.wins.count, losses: vs_results.losses.count }
-    end
+    @stats[:classes][:vs] = Hash[
+      [@vs_hero || Hero.all].flatten.collect do |hero|
+        vs_results = user_results.where(opponent_id: hero.id)
+        vs_results = vs_results.where(hero_id: @as_hero.id) if @as_hero
+        [hero.name, {wins: vs_results.wins.count, losses: vs_results.losses.count}]
+      end.sort_by { |_, hero_stats| win_rate(hero_stats[:wins], hero_stats[:losses]) }
+    ]
 
     num_wins_per_arena = user_arenas
       .joins("LEFT JOIN results ON results.arena_id = arenas.id AND results.win = #{ActiveRecord::Base::connection.quote(true)}")
       .group('arenas.id', 'arenas.hero_id')
       .count('results.id')
 
+    wins_per_run_and_hero  = {}
     num_wins_per_arena.each do |(_, hero_id), num_wins|
-      key = num_wins
-      run = (@stats[:arena][key] ||= {})
+      run = (wins_per_run_and_hero[num_wins] ||= {})
       run[hero_id] ||= 0
       run[hero_id] += 1
     end
 
-    @stats[:arena].each do |(key, count_per_hero)|
+    @stats[:arena] = Hash[wins_per_run_and_hero.collect do |(wins, count_per_hero)|
       # map hero_id to name
-      @stats[:arena][key] = Hash[count_per_hero.collect { |(hero_id, count)| [Hero.find(hero_id).name, count] }]
-    end
+      [
+        wins,
+        Hash[count_per_hero.collect { |(hero_id, count)| [Hero.find(hero_id).name, count] }.sort_by { |_, count| count } ]
+      ]
+    end.sort]
 
     @stats[:overall][:wins] = user_results.wins.count
     @stats[:overall][:losses] = user_results.losses.count
@@ -83,5 +90,11 @@ class StatsController < ApplicationController
     when :last_30_days
       30.days.ago
     end
+  end
+
+  def win_rate(wins, losses)
+    total = wins + losses
+    return 0 if total == 0
+    return wins.to_f / total
   end
 end
