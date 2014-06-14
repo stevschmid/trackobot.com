fad5bf78b42169216e47e9c14fff1f3161f0d73c
Steven Schmid <steschmid@gmx.ch>
2014-06-14 15:19:12 +0200

    first draft

--- app/assets/javascripts/bootstrap.js.coffee
+++ app/assets/javascripts/bootstrap.js.coffee
@@ -1,3 +1,7 @@
 $(document).on 'ready page:load', ->
   $("a[rel~=popover], .has-popover").popover()
   $("a[rel~=tooltip], .has-tooltip").tooltip()
+
+  # close other popovers on click
+  $('.has-popover').on 'click', -> 
+    $('.has-popover').not(this).popover('hide').next('.popover').remove()
--- app/helpers/history_helper.rb
+++ app/helpers/history_helper.rb
@@ -12,4 +12,40 @@ module HistoryHelper
     table.html_safe
   end
 
+  def timeline(result)
+    chronological_card_history = result.card_histories.order(:created_at)
+
+    history_by_player = {'me' => [], 'opponent' => []}
+
+    current_plays = []
+    current_player = nil
+
+    chronological_card_history.each do |card_history|
+      if current_player && current_player != card_history.player
+        history_by_player[current_player] << current_plays
+        current_plays = []
+      end
+      current_player = card_history.player
+      current_plays << card_history
+    end
+
+    if current_plays.any?
+      history_by_player[current_player] << current_plays
+    end
+
+    li = []
+    history_by_player.each do |player, grouped_card_histories|
+      grouped_card_histories.each do |card_histories|
+        history = []
+
+        card_histories.each do |card_history|
+          history << content_tag(:div, "<div class='mana'>#{card_history.card.mana || 0}</div>&nbsp;".html_safe + card_history.card.name, class: 'card')
+        end
+
+        li << content_tag(:li, history.join.html_safe, class: player)
+      end
+    end
+
+    content_tag(:ol, li.join.html_safe, class: 'timeline')
+  end
 end
