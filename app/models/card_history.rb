1f9e5229a45f2ec9612c8cc0b2ba5bb2d21824ee
Steven Schmid <steschmid@gmx.ch>
2014-06-15 02:10:48 +0200

    fix 422 on upload because we added a condition to has_many which causes

--- app/models/card_history.rb
+++ app/models/card_history.rb
@@ -2,7 +2,7 @@ class CardHistory < ActiveRecord::Base
   belongs_to :card
   belongs_to :result
 
-  validates_presence_of :card, :result, :player
+  validates_presence_of :card, :player
   validates_inclusion_of :player, in: %w[me opponent]
 
   scope :me, ->{ where(player: 'me') }
