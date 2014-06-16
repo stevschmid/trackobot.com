7825d19469ea1becb8ca77399bb25980886ecfdb
Steven Schmid <steschmid@gmx.ch>
2014-06-16 13:00:11 +0200

    show '-' for winrates with 0 wins/losses

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -1,5 +1,6 @@
 module StatsHelper
   def win_rate(num_wins, num_losses)
+    return '-' if num_wins + num_losses == 0
     ratio = if num_wins + num_losses > 0
               num_wins.to_f / (num_wins + num_losses)
             else
