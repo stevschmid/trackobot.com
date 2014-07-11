49537499daa4f171dbd6f60b100edb61adae810b
Steven Schmid <steschmid@gmx.ch>
2014-07-11 17:33:21 +0200

    enable rememberable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :trackable
+  devise :database_authenticatable, :trackable, :rememberable
 
   has_many :results
   has_many :arenas
