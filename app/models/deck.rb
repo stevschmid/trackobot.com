84cbd136ab39f1e5af344dfe7afd9aa14b110cfd
Steven Schmid <steschmid@gmx.ch>
2014-07-11 17:27:46 +0200

    some hints on how to use decks

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -3,7 +3,6 @@ class Deck < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :name
-  validates_uniqueness_of :name, scope: :user_id, case_sensitive: false
 
   has_and_belongs_to_many :cards
 
