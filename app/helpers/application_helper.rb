0e8a7d55515da5e81f9ba865aecbb61e12fb10f9
Steven Schmid <steschmid@gmx.ch>
2014-08-19 15:27:54 +0200

    move helper functions around

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def profile_page(title, footer_info = {}, &block)
-    title(title)
+    content_for(:title, title)
     content_tag(:div, class: 'container') do
       [
         render(partial: '/layouts/header'),
@@ -22,43 +22,8 @@ module ApplicationHelper
     end
   end
 
-  def hero_label(hero_name, label = hero_name, additions = {})
-    [
-      hero_icon(hero_name),
-      content_tag(:span, label, additions)
-    ].join(' ').html_safe
-  end
-
-  def player_label_for_result(result, additions = {})
-    if result.deck
-      label_for_deck(result.deck, additions)
-    else
-      hero_label(result.hero.name, result.hero.name, additions)
-    end
-  end
-
-  def opponent_label_for_result(result, additions = {})
-    if result.opponent_deck
-      label_for_deck(result.opponent_deck, additions)
-    else
-      hero_label(result.opponent.name, result.opponent.name, additions)
-    end
-  end
-
-  def label_for_deck(deck, additions = {})
-    hero_label(deck.hero.name, deck.name, additions)
-  end
-
-  def hero_icon(hero_name, additions = {})
-    image_tag("classes/#{hero_name.downcase}.png", {width: 20}.merge(additions))
-  end
-
   def show_feedback_button?
     current_user.feedbacks.where('created_at > ?', 1.day.ago).none?
-  end
-
-  def title(title)
-    content_for :title, title
   end
 end
 
--- app/helpers/history_helper.rb
+++ app/helpers/history_helper.rb
@@ -31,6 +31,37 @@ module HistoryHelper
     }
   end
 
+  def hero_label(hero_name, label = hero_name, additions = {})
+    [
+      hero_icon(hero_name),
+      content_tag(:span, label, additions)
+    ].join(' ').html_safe
+  end
+
+  def player_label_for_result(result, additions = {})
+    if result.deck
+      label_for_deck(result.deck, additions)
+    else
+      hero_label(result.hero.name, result.hero.name, additions)
+    end
+  end
+
+  def opponent_label_for_result(result, additions = {})
+    if result.opponent_deck
+      label_for_deck(result.opponent_deck, additions)
+    else
+      hero_label(result.opponent.name, result.opponent.name, additions)
+    end
+  end
+
+  def label_for_deck(deck, additions = {})
+    hero_label(deck.hero.name, deck.name, additions)
+  end
+
+  def hero_icon(hero_name, additions = {})
+    image_tag("classes/#{hero_name.downcase}.png", {width: 20}.merge(additions))
+  end
+
   private
 
   def group_card_histories_by_card_and_sort_by_mana(card_histories)
