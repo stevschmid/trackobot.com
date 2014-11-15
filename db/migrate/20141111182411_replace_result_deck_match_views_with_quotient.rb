90fc1d92098667779062af7b9bcb3d099f00705a
Jean-Louis Fuchs <ganwell@fangorn.ch>
2014-11-15 12:34:10 +0000

    Better match quality:
    * Filtering non-playable cards in result count
    * Ignoring decks with the suffix (ME) for player 1
     -> I don't want oppenents to match on my decks

--- db/migrate/20141111182411_replace_result_deck_match_views_with_quotient.rb
+++ db/migrate/20141111182411_replace_result_deck_match_views_with_quotient.rb
@@ -16,15 +16,33 @@ class ReplaceResultDeckMatchViewsWithQuotient < ActiveRecord::Migration
              (
                 SELECT COUNT(DISTINCT c_card_histories.card_id)
                 FROM card_histories AS c_card_histories
-				JOIN cards AS c_cards ON c_card_histories.card_id = c_cards.id
+                JOIN cards AS c_cards ON c_card_histories.card_id = c_cards.id
                 WHERE c_card_histories.result_id = results.id AND c_card_histories.player = 0
-				  AND c_cards.ref != 'GAME_005'
+                  AND c_cards.ref != 'GAME_005'   -- the coin (not in card model but needed here)
+                  AND c_cards.ref != 'EX1_165t1' -- druid of the claw minion
+                  AND c_cards.ref != 'EX1_165t2'  -- druid of the claw minion
+                  AND c_cards.ref != 'CS2_mirror' -- mirror image minion
+                  AND c_cards.ref != 'ds1_whelptoken' -- whelp
+                  AND c_cards.ref != 'EX1_116t' -- whelp
+                  AND c_cards.ref != 'TU4c_006' -- bananas
+                  AND c_cards.ref != 'TU4c_006e' -- bananas
+                  AND c_cards.ref != 'EX1_158t' -- treant
+                  AND c_cards.ref != 'EX1_573t' -- treant
+                  AND c_cards.ref != 'EX1_tk9' -- treant
+                  AND c_cards.ref != 'NEW1_040t' -- gnoll
+                  AND c_cards.ref != 'TU4a_003' -- gnoll
+                  AND c_cards.ref != 'tt_010a' -- spellbender minion
+                  AND c_cards.ref != 'FP1_007t' -- nerubian
+                  AND c_cards.ref != 'NAX1_03' -- nerubian
+                  AND c_cards.ref != 'NAX2_05' -- worshipper
+                  AND c_cards.ref != 'NAX2_05H'-- worshipper
              ) AS cards_count_result
       FROM results
         JOIN decks ON decks.user_id = results.user_id
         JOIN cards_decks ON cards_decks.deck_id = decks.id
         JOIN card_histories ON card_histories.result_id = results.id
           AND card_histories.card_id = cards_decks.card_id
+		  AND (card_histories.player = 0 OR decks.name not like '%(ME)')
           AND ((card_histories.player = 0 AND decks.hero_id = results.hero_id) OR (card_histories.player = 1 AND decks.hero_id = results.opponent_id))
       GROUP BY results.id,
                results.user_id,
