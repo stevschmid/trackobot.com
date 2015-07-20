5c65d789d99351a085f4931359db0d037552eefd
Steven Schmid <stevschmid@gmail.com>
2015-07-20 12:30:42 +0200

    Style

--- spec/models/result_spec.rb
+++ spec/models/result_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe Result do
   let(:result) { FactoryGirl.create(:result) }
 
-  describe "arena result" do
+  describe 'arena result' do
     before { result.arena! }
 
-    it "should not be deleted until we figure out how to manage arena results" do
+    it 'should not be deleted until we figure out how to manage arena results' do
       expect{result.destroy}.to_not change(Result, :count).by(-1)
     end
   end
 
-  describe "card_history associations" do
+  describe 'card_history associations' do
     let!(:first_play) do
       FactoryGirl.create(:card_history, result: result)
     end
@@ -20,11 +20,11 @@ describe Result do
       FactoryGirl.create(:card_history, result: result)
     end
 
-    it "should have the right card_histories in the right order" do
+    it 'should have the right card_histories in the right order' do
       expect(result.card_histories.to_a).to eq [first_play, second_play]
     end
 
-    it "should destroy associated card_histories" do
+    it 'should destroy associated card_histories' do
       card_histories = result.card_histories.to_a
       result.destroy
       expect(card_histories).to_not be_empty
@@ -33,10 +33,10 @@ describe Result do
       end
     end
 
-    describe "arena result" do
+    describe 'arena result' do
       before { result.arena! }
 
-      its "card_histories should not be deleted" do
+      its 'card_histories should not be deleted' do
         card_histories = result.card_histories.to_a
         result.destroy
         expect(card_histories).to_not be_empty
