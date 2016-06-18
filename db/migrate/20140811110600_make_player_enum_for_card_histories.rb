6323020206ff8919a770c92883112505f9fa120e
Steven Schmid <stevschmid@gmail.com>
2016-06-18 16:37:59 +0200

    Fix migrations

--- db/migrate/20140811110600_make_player_enum_for_card_histories.rb
+++ db/migrate/20140811110600_make_player_enum_for_card_histories.rb
@@ -2,8 +2,10 @@ class MakePlayerEnumForCardHistories < ActiveRecord::Migration
   def change
     add_column :card_histories, :player_enum, :integer
 
-    CardHistory.reset_column_information
-    CardHistory.where(player: 'me').update_all(player_enum: 0)
-    CardHistory.where(player: 'opponent').update_all(player_enum: 1)
+    if defined? CardHistory
+      CardHistory.reset_column_information
+      CardHistory.where(player: 'me').update_all(player_enum: 0)
+      CardHistory.where(player: 'opponent').update_all(player_enum: 1)
+    end
   end
 end
--- db/migrate/20140811112001_migrate_player_from_card_histories.rb
+++ db/migrate/20140811112001_migrate_player_from_card_histories.rb
@@ -1,8 +1,10 @@
 class MigratePlayerFromCardHistories < ActiveRecord::Migration
   def change
-    CardHistory.where(player_enum: nil).where(player: 'me').update_all(player_enum: 0)
-    CardHistory.where(player_enum: nil).where(player: 'opponent').update_all(player_enum: 1)
-    remove_column :card_histories, :player
-    rename_column :card_histories, :player_enum, :player
+    if defined? CardHistory
+      CardHistory.where(player_enum: nil).where(player: 'me').update_all(player_enum: 0)
+      CardHistory.where(player_enum: nil).where(player: 'opponent').update_all(player_enum: 1)
+      remove_column :card_histories, :player
+      rename_column :card_histories, :player_enum, :player
+    end
   end
 end
