540c6edff05029e4fb7c2d4bb061eeaf1a0c1186
Steven Schmid <stevschmid@gmail.com>
2016-12-21 02:03:03 +0100

    Waste

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -5,10 +5,6 @@ class Deck < ApplicationRecord
 
   validates_presence_of :hero, :name
 
-  def self.reset_all_classifiers!
-    Deck.update_all(classifier: nil)
-  end
-
   def full_name
     "#{name} #{hero.name}"
   end
