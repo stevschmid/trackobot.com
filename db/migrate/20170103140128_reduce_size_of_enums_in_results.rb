d78ec214b77283584b01843feb0a84dacfd6daec
Steven Schmid <stevschmid@gmail.com>
2017-01-03 16:54:38 +0100

    Revert reduce size

--- db/migrate/20170103140128_reduce_size_of_enums_in_results.rb
+++
@@ -1,7 +0,0 @@
-class ReduceSizeOfEnumsInResults < ActiveRecord::Migration[5.0]
-  def change
-     # one byte is enough for enum
-    change_column :results, :hero, :integer, limit: 1
-    change_column :results, :opponent, :integer, limit: 1
-  end
-end
