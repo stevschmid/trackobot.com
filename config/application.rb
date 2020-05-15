12bbe48d120c33f89cf75d57de9a48ce8984915d
Jill Shriver <jill_shriver@icloud.com>
2020-05-15 09:45:25 -0400

    Adds boilerplate code generated by Responders gem setup.

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ require 'csv'
 
 module Trackobot
   class Application < Rails::Application
+    # Use the responders controller from the responders gem
+    config.app_generators.scaffold_controller :responders_controller
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
